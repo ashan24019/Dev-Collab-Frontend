67dc178f900b2c16cde0277b7af4511ad99d6736
Ashan <ashanvimodh241@gmail.com>
2026-06-19 01:25:45 +0530

    feat: add member management to project detail page

--- src/api/projectApi.js
+++ src/api/projectApi.js
@@ -14,3 +14,13 @@ export const getProjectById = async (id) => {
     const response = await axiosInstance.get(`/api/projects/${id}`);
     return response.data;
 }
+
+export const addMemberToProject = async (projectId, userId) => {
+    const response = await axiosInstance.patch(`/api/projects/${projectId}/members/${userId}`);
+    return response.data;
+};
+
+export const getAllUsers = async () => {
+    const response = await axiosInstance.get('/api/users');
+    return response.data;
+};
